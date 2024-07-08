8358226d2d4412907ef6f75929d7a213b2f586cf
jamesjo <jamescakr@gmail.com>
2024-07-08 10:01:17 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const API_KEY = `d2bb588d15db43cabd709a13297a0071`;
+let news = [];
+const getLatestNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log("dddd", news);
+};
+getLatestNews();
