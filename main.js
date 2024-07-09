9695a22d219d840c3dc92e075252e155070bef11
jamesjo <jamescakr@gmail.com>
2024-07-09 17:59:15 -0600

    chapter 7 & 8

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
-// const API_KEY = `d2bb588d15db43cabd709a13297a0071`;
-// const apiKey =
-//   "http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines";
-
-// let page = 2;
-// let category = "";
-// let keyword = "";
-// let pageSize = 10;
-// let news = [];
-
-// const getLatestNews = async () => {
-//   //   const url = new URL(
-//   //     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-//   //   );
-//   const url = new URL(
-//     `https://james-news-project.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}${category}${keyword}`
-//   );
-
-//   //   const url = new URL(``);
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   news = data.articles;
-//   console.log("dddd", news);
-// };
-// getLatestNews();
-
-let news = [];
-const pageSize = 10;
+const API_KEY = `d2bb588d15db43cabd709a13297a0071`;
+let newsList = [];
 let page = 2;
 let category = "";
 let keyword = "";
+let pageSize = 10;
+
 const getLatestNews = async () => {
+  //   const url = new URL(
+  //     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  //   );
+
   const url = new URL(
     `https://james-news-project.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}${category}${keyword}`
   );
 
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("news=", news);
+  newsList = data.articles;
+  render();
+  console.log("dddd", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news-section">
+                <div class="col-lg-4">
+                    <img class="news-img-size"
+                        src=${news.urlToImage} />
+                </div>
+                <div class="col-lg-8">
+                    <h2>
+                        ${news.title}
+                    </h2>
+                    <p>
+                        ${news.description}
+                    </p>
+                    <div>${news.source.name} * ${news.publishedAt}</div>
+                </div>
+            </div>`
+    )
+    .join("");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
