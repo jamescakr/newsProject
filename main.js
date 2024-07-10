02fcefff61c67d0def6111fe146317e172480bb2
jamesjo <jamescakr@gmail.com>
2024-07-09 18:36:39 -0600

    chapter 7 & 8 with homework

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ let keyword = "";
 let pageSize = 10;
 
 const getLatestNews = async () => {
-  //   const url = new URL(
-  //     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-  //   );
+  // const url = new URL(
+  //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  // );
 
   const url = new URL(
     `https://james-news-project.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}${category}${keyword}`
@@ -27,16 +27,30 @@ const render = () => {
       (news) => `<div class="row news-section">
                 <div class="col-lg-4">
                     <img class="news-img-size"
-                        src=${news.urlToImage} />
+                        src=${
+                          news.urlToImage
+                            ? news.urlToImage
+                            : "https://cdn.vectorstock.com/i/500p/32/45/no-image-symbol-missing-available-icon-gallery-vector-45703245.jpg"
+                        } />
                 </div>
                 <div class="col-lg-8">
                     <h2>
                         ${news.title}
                     </h2>
                     <p>
-                        ${news.description}
+                        ${
+                          news.description
+                            ? news.description.length > 200
+                              ? news.description.slice(0, 200) + "..."
+                              : news.description
+                            : "No content available"
+                        }
                     </p>
-                    <div>${news.source.name} * ${news.publishedAt}</div>
+                    <div>${
+                      news.source && news.source.name
+                        ? news.source.name
+                        : "No Source"
+                    } * ${moment(news.publishedAt).fromNow()}</div>
                 </div>
             </div>`
     )
